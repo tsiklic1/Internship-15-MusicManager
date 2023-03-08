e42c7881e0e71bc435da92c675ed8fd54fc028a9
Toma Siklic <toma.siklic@gmail.com>
2023-03-08 21:30:30 +0100

    adding display of albums (sorted)

--- music-manager/src/components/DisplayAlbums.jsx
+++ music-manager/src/components/DisplayAlbums.jsx
@@ -1,9 +1,32 @@
 const DisplayAlbums = ({albumsList}) => {
+  albumsList.sort((a,b) => {
+    let y1 = a.year;
+    let y2 = b.year;
+
+    let a1 = a.author;
+    let a2 = b.author;
+
+    let t1 = a.title;
+    let t2 = b.title;
+
+    if(y1<y2) return -1;
+    if(y1>y2) return 1;
+
+    if(a1<a2) return -1;
+    if(a1>a2) return 1;
+
+    if(t1<t2) return -1;
+    if(t1>t2) return 1;
+    return 0
+  })
+
+  let today = new Date()
 
   return (
     <div>
-      {albumsList.map((album)=> 
-      <p key = {album.id}> {JSON.stringify(album)}</p>)}
+      {albumsList.map((album)=> {
+      return <p className={(album.addDate.getMonth() === today.getMonth() && album.addDate.getDay() === today.getDay() && album.addDate.getFullYear() === today.getFullYear()) ? "album-added-today" : ""} 
+      key = {album.id}> {JSON.stringify(album)}</p>})}
     </div>
   )
 }
--- music-manager/src/components/Form.jsx
+++ music-manager/src/components/Form.jsx
@@ -10,12 +10,20 @@ const Form = ({handleAddAlbum})=>{
   const [year, setYear] = useState("")
   const [emptyFieldExists, setEmptyFieldExists] = useState(false)
 
+  const handleChangeOnYear = (value) => {
+    if(isNaN(+value)){
+      console.log("Input is not number")
+      return
+    }
+    setYear(+value)
+  }
+
   const handleSubmit = (e)=>{
-    console.log("Uslo u handleSubmit")
     console.log(genre)
     
     setEmptyFieldExists(false)
-    if(!title.length || !author.length|| !year.length || !genre.length){
+
+    if(!title.length || !author.length || !genre.length || year === ""){
       console.log("something is empty")
       setEmptyFieldExists(true)
       return;
@@ -36,7 +44,7 @@ const Form = ({handleAddAlbum})=>{
     <p>Album info</p>
     <input className="album-info-input" type="text " placeholder="Title" value ={title} onChange = {e => setTitle(e.target.value)} />
     <input className="album-info-input" type="text " placeholder="Author" value ={author} onChange = {e => setAuthor(e.target.value)} />
-    <input className="album-info-input" type="text " placeholder="Year" value ={year} onChange = {e => setYear(e.target.value)} />
+    <input className="album-info-input" type="text " placeholder="Year" value ={year} onChange = {e => handleChangeOnYear(e.target.value)} />
     <GenreDropdown onSubmit = {getData}/>
     
      <button onClick={(e) => handleSubmit(e)}>Add</button>
