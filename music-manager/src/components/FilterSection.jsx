6656e7d09659da5ecd5185a80547e279e35cc67f
Toma Siklic <toma.siklic@gmail.com>
2023-03-09 18:18:41 +0100

    styling filter section

--- music-manager/src/components/FilterSection.jsx
+++ music-manager/src/components/FilterSection.jsx
@@ -2,13 +2,13 @@ import { genres } from "../lists"
 
 const FilterSection = ({changeTitleFilter, changeGenreFilter}) => {
   return (
-    <div>
-      <p>Filter</p>
+    <div className="filter-wrapper">
+      <h2 className="filter-wrapper__title">Filter</h2>
       <input className="filter-input" onChange={e => changeTitleFilter(e.target.value.toLowerCase())}  placeholder="By title"/>
       <div>
-        <button key="none" value={""} onClick={e => changeGenreFilter(e.target.value)}>None</button>
+        <button className="genre-filter-button" key="none" value={""} onClick={e => changeGenreFilter(e.target.value)}>None</button>
         {genres.map(genre =>
-           <button value = {genre} onClick={e => changeGenreFilter(e.target.value)} key = {genre}>{genre}</button>)}
+           <button className="genre-filter-button" value = {genre} onClick={e => changeGenreFilter(e.target.value)} key = {genre}>{genre}</button>)}
       </div>
     </div>
   )
